1854493214b7de59db1a96602fa77be85bd35075
Tom Savage <tcsavage@gmail.com>
2015-05-30 17:50:14 +0100

    Some documentation for internal functions

--- src/Collide/Internal.hs
+++ src/Collide/Internal.hs
@@ -5,8 +5,10 @@ import Data.Monoid
 import Linear
 import Prelude hiding (minimum, maximum, sum)
 
+-- | Project normal along each of the given vectors.
 projectVS :: (Functor t, Foldable v, Metric v, Floating a) => t (v a) -> v a -> t a
 projectVS vs n = fmap (sum . project n) vs
 
+-- | Do the given sets of points overlap?
 overlapping :: (Foldable t, Ord a) => t a -> t a -> All
 overlapping as bs = All $ not (maximum as < minimum bs || maximum bs < minimum as)
